4abe1c50b3e954dc08e2f96d34607ded945dcd98
diced <pranaco2@gmail.com>
2021-07-19 17:25:08 -0700

    remove all russian stuff and change to jetbrains

--- JetbrainsMonoFont.cs
+++ JetbrainsMonoFont.cs
@@ -1,17 +1,58 @@
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
 using ReLogic.Graphics;
 using Terraria;
+using Terraria.ModLoader;
 
-namespace BetterRussian
+namespace JetbrainsMonoFont
 {
-	partial class BetterRussianMod
+	public class JetbrainsMonoFont : Mod
 	{
+		private TaskCompletionSource<bool> unloadTCS;
 		private bool hasBackups;
 		private DynamicSpriteFont[] combatTextBackup;
 		private DynamicSpriteFont deathTextBackup;
 		private DynamicSpriteFont itemStackBackup;
 		private DynamicSpriteFont mouseTextBackup;
+		
+		public override void Load()
+		{
+			if(Main.dedServ) {
+				return;
+			}
 
+			LoadFonts();
+
+			Main.OnPreDraw += PreDraw;
+		}
+		public override void Unload()
+		{
+			if(Main.dedServ) {
+				return;
+			}
+
+			unloadTCS = new TaskCompletionSource<bool>();
+
+			unloadTCS.Task.Wait(); //Graphics have to be unloaded in PreDraw on the main thread, otherwise they may get unloaded as they're used.
+
+			Main.OnPreDraw -= PreDraw;
+		}
+
+		private void PreDraw(GameTime obj)
+		{
+			if(unloadTCS!=null) {
+				UnloadGraphics();
+
+				unloadTCS.SetResult(true);
+
+				unloadTCS = null;
+			}
+		}
+		private void UnloadGraphics()
+		{
+			UnloadFonts();
+		}
+		
 		private void LoadFonts()
 		{
 			combatTextBackup = Main.fontCombatText;
